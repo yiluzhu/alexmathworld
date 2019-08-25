b70ae10c6c989a0709403ee7270eb8fa4a055639
Yilu Zhu <zhuyilu@hotmail.com>
2019-08-25 10:53:04 +0100

    support passcode

--- question.py
+++ question.py
@@ -11,7 +11,7 @@ class Question:
             'extreme': (11, 40),
         }
 
-    def generate_questions(self, difficulty=None, number=None):
+    def generate_questions(self, difficulty, number):
         questions = []
         start, end = self.difficulty_dict[difficulty]
         for i in range(number):
@@ -22,19 +22,18 @@ class Question:
             op = '+' if random.choice([True, False]) else '-'
             div_str = self.create_division(c, d)
             question_str = f'{a} x {b} {op} {div_str}'
-            #answer = eval(question_str.replace('x', '*').replace('&divide;', '/'))
-            answer = eval(question_str.replace('x', '*').replace('\u00f7', '/'))
             questions.append(question_str)
         return questions
 
     def create_division(self, c, d):
         mul = c * d
-        #return f'{mul} &divide; {c}'
         return f'{mul} \u00f7 {c}'
 
     def check_answers(self, questions, submitted_answers):
         results = []
+        correct_answers = []
         for question, submit_answer in zip(questions, submitted_answers):
+            correct_answer = eval(question.replace('x', '*').replace('\u00f7', '/'))
             if submit_answer == passcode:
                 correctness = True
             else:
@@ -43,7 +42,7 @@ class Question:
                 except ValueError:
                     correctness = False
                 else:
-                    correct_answer = eval(question.replace('x', '*').replace('\u00f7', '/'))
                     correctness = correct_answer == submit_answer
             results.append(correctness)
-        return results
+            correct_answers.append(correct_answer)
+        return correct_answers, results
--- react_api.py
+++ react_api.py
@@ -9,24 +9,21 @@ CORS(app)
 qu = Question()
 
 
-@app.route('/questions', methods=['GET', 'POST'])
+@app.route('/questions', methods=['POST'])
 def show_questions():
-    if request.method == 'GET':
-        question_list = qu.generate_questions()
-    else:
-        body = json.loads(request.data)
-        difficulty = body.get('difficulty')
-        number = int(body.get('number'))
-        question_list = qu.generate_questions(difficulty, number)
+    body = json.loads(request.data)
+    difficulty = body.get('difficulty')
+    number = int(body.get('number'))
+    question_list = qu.generate_questions(difficulty, number)
     return Response(json.dumps(question_list), 200, mimetype='application/json')
 
 
 @app.route('/result', methods=['POST'])
 def check_answer():
     body = json.loads(request.data)
-    print('answers are', body.get('answers'))
-    result = qu.check_answers(body.get('questions'), body.get('answers'))
-    return Response(json.dumps(result), 200, mimetype='application/json')
+    correct_answers, result = qu.check_answers(body.get('questions'), body.get('submittedAnswers'))
+    return Response(json.dumps({'correct_answers': correct_answers, 'result': result}),
+                    200, mimetype='application/json')
 
 
 app.run()
